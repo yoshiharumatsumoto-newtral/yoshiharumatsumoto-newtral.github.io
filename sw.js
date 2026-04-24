6123e2763e10bfc46935f1b86a7eb83b560b428f
yoshiharumatsumoto-newtral <yoshiharu_matsumoto@matsumotokousakusyo.com>
2026-04-24 18:38:45 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,8 +1,21 @@
-const CACHE = 'fureru-v1';
-const FILES = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
+const CACHE = 'fureru-v2';
+const FILES = [
+  '/fureru/',
+  '/fureru/index.html',
+  '/fureru/manifest.json',
+  '/fureru/icon-192.png',
+  '/fureru/icon-512.png'
+];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
 });
 
 self.addEventListener('fetch', e => {
